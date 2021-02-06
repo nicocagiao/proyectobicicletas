f4020b21b108093c7354dedbac736fd409cdacc1
Nico <nicolascagiao@hotmail.com>
2021-02-06 13:55:23 -0300

    sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,39 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'garth.mckenzie@ethereal.email',
-        pass: 'N1fZ8nYrrUMSptaVYe'
+
+let mailConfig;
+
+if (process.env.NODE_ENV === 'production') {
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+
+    mailConfig = sgTransport(options);
+
+} else {
+    if (process.env.NODE_ENV === 'staging') {
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+
+        mailConfig = sgTransport(options);
+
+    } else {
+        // Development
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ETHEREAL_USER,
+                pass: process.env.ETHEREAL_PWD
+            }
+        };
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
