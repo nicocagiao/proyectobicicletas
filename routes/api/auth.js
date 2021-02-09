19255c7ed15a5059709e0c2e6e089d7f0894db60
Nico <nicolascagiao@hotmail.com>
2021-02-09 12:18:05 -0300

    passport fix

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('../../config/passport');
 
 const authController = require('../../controllers/api/authControllerAPI');
 
